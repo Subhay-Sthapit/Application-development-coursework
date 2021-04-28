4bc78b9ddaa97735fec710ddc41397f2dd108f31
subhay999 <68510957+Subhay-Sthapit@users.noreply.github.com>
2021-04-29 03:32:43 +0545

    created crud of Category Model

--- StockManagementSystem/Data/ApplicationDbContext.cs
+++ StockManagementSystem/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using StockManagementSystem.Models;
 
 namespace StockManagementSystem.Data
 {
@@ -12,5 +13,6 @@ namespace StockManagementSystem.Data
             : base(options)
         {
         }
+        public DbSet<StockManagementSystem.Models.Category> Category { get; set; }
     }
 }
